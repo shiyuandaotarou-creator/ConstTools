184fc68419e2de2221eb474d0442e456cafc2be9
shiyuandaotarou-creator <shiyuandaotarou@gmail.com>
2026-08-19 18:18:03 +0900

    URLコンバータ修正

--- url_converter/main.js
+++ url_converter/main.js
@@ -2,41 +2,60 @@ const inputURL = document.getElementById('inputURL');
 const outputURL = document.getElementById('outputURL');
 const copyButton = document.getElementById('copyButton');
 const clearButton = document.getElementById('clearButton');
+const pasteButton = document.getElementById('pasteButton');
 const copyMode = document.getElementById('copyMode');
 let url;
 let resultUrl;
-let copyModeValue = copyMode.value;
-inputURL.onchange = () => {
-    url = inputURL.value.replace("https://github.com/","https://cdn.jsdelivr.net/gh/").replace("/blob/", "@");
-    outputURL.value = url;
-}
-clearButton.onclick = ()=>{
-    inputURL.value = "";
-    outputURL.value= "";
-}
 copyButton.onclick = () => {
-    switch (copyModeValue) {
-        case "simple":
-            resultUrl = url;
-        case "asModule":
-            resultUrl = "import * as moduleName from "+"'"+url +"';";
-        case "asClass":
-            resultUrl = "import className from "+"'"+url +"';";
+    copy();
+}
+inputURL.onchange = () => {
+    url = inputURL.value.replace("https://github.com/", "https://cdn.jsdelivr.net/gh/").replace("/blob/", "@");
+    outputURL.value = url;
+    copyButton.textContent = "Copy";
+}
+pasteButton.onclick = () => {
+    navigator.clipboard.readText().then(res => {
+        inputURL.value = res
+        url = inputURL.value.replace("https://github.com/", "https://cdn.jsdelivr.net/gh/").replace("/blob/", "@");
+        outputURL.value = url;
+        copy();
+        copyButton.textContent = "Copy";
+    });
+}
+clearButton.onclick = () => {
+    inputURL.value = "";
+    outputURL.value = "";
+}
+function copy() {
+    console.log(copyMode.value);
+    if (copyMode.value === "simple") {
+        console.log("シンプル");
+    
+        resultUrl = url;
+        outputURL.value = resultUrl;
+    } else if (copyMode.value === "asModule") {
+        console.log("モジュール");
+        resultUrl = "import * as moduleName from " + "'" + url + "';";
+        outputURL.value = resultUrl;
+    } else if (copyMode.value === "asClass") {
+        console.log("クラス");
+        resultUrl = "import className from " + "'" + url + "';";
+        outputURL.value = resultUrl;
     }
     if (url !== undefined) {
         navigator.clipboard.writeText(resultUrl).then(() => {
+            console.log(resultUrl);
             console.log("コピー成功");
             copyButton.textContent = "Copied!";
         }).catch((e) => {
             console.log("コピー失敗", e);
         })
-    }else{
+    } else {
         console.log("trying to copy something undefined");
         console.log("コピー失敗");
     }
 }
-copyMode.onchange = ()=>{
-    copyModeValue = copyMode.value;
-    console.log(copyModeValue);
-    
+copyMode.onchange = () => {
+    copyButton.textContent = "Copy"
 }
